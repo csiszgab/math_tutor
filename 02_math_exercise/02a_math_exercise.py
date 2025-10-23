6cfeccdc89d8a7b6ac86b44c3e6cb563b4470af1
Csiszár Gábor <csiszar005gabor@gmail.com>
2025-10-23 16:45:47 +0200

    Modifications to be more user friendly: multiplication limited to six, addition limited to 8, filtering
    	1. random numbers generated bw 1 and 8
            2. either addition or multiplication chosen
    	3. if operator is multiplication and one of the numbers is
               greater than 6 => it is thrown away (filtering)
    	4. shorter sleep intervals
    	5. scrolling only allowed if the result is two digit number

--- 02_math_exercise/02a_math_exercise.py
+++ 02_math_exercise/02a_math_exercise.py
@@ -4,22 +4,26 @@ import random
 
 while True:
     if button_a.was_pressed():
-        a = random.randint(1 , 5)
-        b = random.randint(1 , 5)
-        op = random.randint(1 , 2)
+        filter = True
+        while filter:
+            a = random.randint(1, 8)
+            b = random.randint(1, 8)
+            op = random.randint(1, 2)
+            if not (op == 2 and (a > 6 or b > 6)):
+                filter = False
         if op == 1:
-            ops = '+'
+            ops = "+"
         elif op == 2:
-            ops = '*'
-        if ops == '+':
-            res = a+b
-        elif ops == '*':
-            res = a*b
-        display.scroll(str(a) + ops + str(b) + '=?')
-        sleep(2000)
+            ops = "*"
+        if ops == "+":
+            res = a + b
+        elif ops == "*":
+            res = a * b
+        display.scroll(str(a) + ops + str(b) + "=?")
+        sleep(1000)
         counter = 0
         while not (button_a.was_pressed()):
-            sleep(1000)
+            sleep(500)
             counter = counter + button_b.get_presses()
             if counter < 10:
                 display.show(str(counter))
@@ -29,5 +33,3 @@ while True:
             display.show(Image.HAPPY)
         else:
             display.show(Image.SAD)
-
-
